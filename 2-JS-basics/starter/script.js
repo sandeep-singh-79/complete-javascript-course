1eeec3655807600716576c88975b022329ffadd9
Sandeep Singh <s.sandeep79@ymail.com>
2020-07-08 16:34:39 +0530

    Coding Challenge 2

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -274,14 +274,73 @@ switch (true) {
 // called so as they evaluate to false when evaluated in if construct
 // Truthy values: NOT falsy values
 
-let height;
+/* let height;
 height = 23;
 
 if (height || height === 0) console.log(`variable is defined`);
 else console.log("variable is not defined");
 
 
-if (height === '23') console.log('The == operator does type coercion!')
+if (height === '23') console.log('The == operator does type coercion!') */
 // the == operator uses type coercion during comparision
 // the === operator/strict equality operator compares the objects as is
 // without any type coercion.
+
+/*****************************
+ * CODING CHALLENGE 2
+ */
+
+/*
+John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+
+1. Calculate the average score for each team
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
+
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+
+GOOD LUCK 😀
+*/
+/* let john_team,
+  mike_team,
+  mary_team,
+  john_team_avg,
+  mike_team_avg,
+  mary_team_avg;
+john_team = [89, 120, 103];
+mike_team = [116, 94, 123];
+mary_team = [97, 134, 105];
+
+function get_team_sum(team) {
+  let sum = 0;
+  team.forEach((element) => {
+    sum += element;
+  });
+  return sum;
+}
+
+function get_team_avg(team) {
+  return get_team_sum(team) / team.length;
+}
+
+john_team_avg = get_team_avg(john_team);
+console.log(`Avg score of John's team is: ${john_team_avg}`);
+mike_team_avg = get_team_avg(mike_team);
+console.log(`Avg score of Mike's team is: ${mike_team_avg}`);
+mary_team_avg = get_team_avg(mary_team);
+console.log(`Avg score of Mary's team is: ${mary_team_avg}`);
+
+if (john_team_avg > mike_team_avg && john_team_avg > mary_team_avg)
+  console.log(`John's team wins with ${john_team_avg} score`);
+else if (mike_team_avg > john_team_avg && mike_team_avg > mary_team_avg)
+  console.log(`Mike's team wins with ${mike_team_avg} score`);
+else if (mary_team_avg > john_team_avg && mike_team_avg < mary_team_avg)
+  console.log(`Mary's team wins with ${mary_team_avg} score`);
+else if (
+  john_team_avg === mike_team_avg ||
+  john_team_avg == mary_team_avg ||
+  mike_team_avg === mary_team_avg
+)
+  console.log("It's a draw!!!"); */
+  

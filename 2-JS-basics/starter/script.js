d4246751f41ba72ef11e7142b8bf70c4ea8dbd0d
Sandeep Singh <s.sandeep79@ymail.com>
2020-07-09 01:25:55 +0530

    Coding Chanllenge 5

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -550,7 +550,7 @@ Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and heig
 
 GOOD LUCK 😀
 */
-
+/* 
 let john = {
   fullName: "John Smith",
   mass: 110,
@@ -580,4 +580,109 @@ else if (john.bmi < mark.bmi)
 else
   console.log(
     `${mark.fullName} & ${john.fullName} have the same bmi at ${mark.bmi}`
+  );
+  */
+
+/*****************************
+ * Loops and iteration
+ */
+
+/*****************************
+ * CODING CHALLENGE 5
+ */
+
+/*
+Remember the tip calculator challenge? Let's create a more advanced version using everything we learned!
+
+This time, John and his family went to 5 different restaurants. The bills were $124, $48, $268, $180 and $42.
+John likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+Implement a tip calculator using objects and loops:
+1. Create an object with an array for the bill values
+2. Add a method to calculate the tip
+3. This method should include a loop to iterate over all the paid bills and do the tip calculations
+4. As an output, create 1) a new array containing all tips, and 2) an array containing final paid amounts (bill + tip). HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
+
+
+EXTRA AFTER FINISHING: Mark's family also went on a holiday, going to 4 different restaurants. The bills were $77, $375, $110, and $45.
+Mark likes to tip 20% of the bill when the bill is less than $100, 10% when the bill is between $100 and $300, and 25% if the bill is more than $300 (different than John).
+
+5. Implement the same functionality as before, this time using Mark's tipping rules
+6. Create a function (not a method) to calculate the average of a given array of tips. HINT: Loop over the array, and in each iteration store the current sum in a variable (starting from 0). After you have the sum of the array, divide it by the number of elements in it (that's how you calculate the average)
+7. Calculate the average tip for each family
+8. Log to the console which family paid the highest tips on average
+
+GOOD LUCK 😀
+*/
+
+let john = {
+  fullName: "John Smith",
+  bills: new Array(124, 48, 268),
+  calculate_tip: function () {
+    this.tips = [];
+    this.total = [];
+
+    for (const bill of this.bills) {
+      let percentage, tip;
+      switch (true) {
+        case bill > 0 && bill < 50:
+          percentage = 0.2;
+        case bill >= 50 && bill < 200:
+          percentage = 0.15;
+        case bill >= 200:
+          percentage = 0.1;
+      }
+      tip = bill * percentage;
+      this.tips.push(tip);
+      this.total.push(bill + tip);
+    }
+  },
+};
+
+let mark = {
+  fullName: "Mark Miller",
+  bills: new Array(77, 375, 110, 45),
+  calculate_tip: function () {
+    this.tips = [];
+    this.total = [];
+
+    for (const bill of this.bills) {
+      let percentage, tip;
+      switch (true) {
+        case bill > 0 && bill < 50:
+          percentage = 0.2;
+        case bill >= 50 && bill < 200:
+          percentage = 0.15;
+        case bill >= 200:
+          percentage = 0.1;
+      }
+      tip = bill * percentage;
+      this.tips.push(tip);
+      this.total.push(bill + tip);
+    }
+  },
+};
+
+function calculate_tip_avg(tips) {
+  let tip_sum = 0;
+  for (let i = 0; i < tips.length; i++) tip_sum += tips[i];
+  return tip_sum / tips.length;
+}
+
+john.calculate_tip();
+mark.calculate_tip();
+
+john.average = calculate_tip_avg(john.tips);
+mark.average = calculate_tip_avg(mark.tips);
+if (john.average > mark.average)
+  console.log(
+    `John's family paid the higher tip amount at ${john.average} on average`
+  );
+else if (mark.average < mark.average)
+  console.log(
+    `Marks's family paid the higher tip amount at ${mark.average} on average`
+  );
+else
+  console.log(
+    `Both John's and Marks's families paid the same tip amount at ${mark.average} on average`
   );

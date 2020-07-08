d269d4854c6212f702b7e2a61e5632b7a921b162
Sandeep Singh <s.sandeep79@ymail.com>
2020-07-08 10:43:06 +0530

    operator precedence

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -21,8 +21,8 @@ var 3Years = 3 */
  * Variable mutation and type coercion
  */
 
-var firstName = "John";
-var age = 28;
+/* var firstName = "John";
+var age = 28; */
 
 // Type Coercion
 /* console.log(firstName + " " + age);
@@ -61,7 +61,7 @@ console.log(firstName + " " + lastname); */
 /*
  * Basic Operators
  */
-var now, yearJohn, yearMark;
+/* var now, yearJohn, yearMark;
 now = 2018;
 ageJohn = 28;
 ageMark = 33;
@@ -75,7 +75,7 @@ console.log(now * 2);
 console.log(now / 10);
 
 // logical operators
-var johnOlder = /* ageJohn > ageMark */ ageJohn < ageMark;
+var johnOlder =  ageJohn < ageMark;
 console.log(johnOlder);
 
 // typeof Operator
@@ -84,3 +84,43 @@ console.log(typeof ageJohn);
 console.log(typeof "Mark is older than John");
 var x;
 console.log('x is a var of type ' + typeof x);
+*/
+
+/*
+ * operator precedence
+ */
+var now, yearJohn, fullAge;
+now = 2018;
+yearJohn = 1989;
+fullAge = 18;
+
+var isFullAge = yearJohn - now >= fullAge;
+console.log(isFullAge);
+// this works as '-' has higher precedence over the other operators.
+
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = ageJohn + ageMark / 2; // without grouping
+console.log(average);
+var avg = (ageJohn + ageMark) / 2; // with grouping
+console.log(avg);
+
+// multiple assignments
+var x, y;
+// x = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+console.log(x, y); // console.log(x);
+// above works as assignment operator works from right to left
+// while most operators work left to right
+
+// More operators
+x = x * 2;
+x *= x;
+console.log(x);
+
+x += 10;
+console.log(x);
+/* x = x + 1;
+x += 1; */
+x++; // x--
+console.log(x);

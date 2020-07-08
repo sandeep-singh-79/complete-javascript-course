a07fd9b787d7e26da7b432180d3842fbf7f31823
Sandeep Singh <s.sandeep79@ymail.com>
2020-07-08 11:09:32 +0530

    Coding challenge 1 completed with prompts

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -89,7 +89,7 @@ console.log('x is a var of type ' + typeof x);
 /*
  * operator precedence
  */
-var now, yearJohn, fullAge;
+/* var now, yearJohn, fullAge;
 now = 2018;
 yearJohn = 1989;
 fullAge = 18;
@@ -120,7 +120,41 @@ console.log(x);
 
 x += 10;
 console.log(x);
-/* x = x + 1;
-x += 1; */
+// x = x + 1;
+// x += 1;
 x++; // x--
-console.log(x);
+console.log(x); */
+
+/*****************************
+ * CODING CHALLENGE 1
+ */
+
+/*
+* Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+
+* 1. Store Mark's and John's mass and height in variables
+* 2. Calculate both their BMIs
+* 3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+* 4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). 
+*
+* GOOD LUCK 😀
+*/
+// Solution
+let massJohn, massMark, heightJohn, heightMark, bmiJohn, bmiMark;
+massJohn = new Number(prompt('What is John\'s weight? '));
+heightJohn = new Number(prompt('What is John\'s height? '));
+console.log('weight of john: ' + massJohn);
+console.log('height of john: ' + heightJohn);
+massMark = new Number(prompt('What is Mark\'s weight? '));
+heightMark = new Number(prompt('What is Mark\'s height? '));
+console.log("weight of Mark: " + massMark);
+console.log("height of Mark: " + heightMark);
+
+// calculate BMI
+bmiJohn = massJohn / (heightJohn ** 2);
+bmiMark = massMark / (heightMark ** 2);
+console.log("bmi of john: " + bmiJohn);
+console.log("bmi of mark: " + bmiMark);
+
+let isBmiHigher = bmiMark > bmiJohn;
+console.log('Is Mark\'s BMI higher than John\'s? ' + isBmiHigher);

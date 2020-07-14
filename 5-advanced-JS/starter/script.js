617abcfda11adb5bfb32f2762058b9a2cac1ef71
Sandeep Singh <s.sandeep79@ymail.com>
2020-07-14 11:43:13 +0530

    Section 5 - Object.create

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -35,3 +35,33 @@ john.calc_age();
 jane.calc_age();
 mark.calc_age(); */
 
+/////////////////////////////
+// Lecture: Object.create
+/* 
+// create an object prototype first
+var personProto = {
+  calculate_age: function () {
+    console.log(new Date().getFullYear() - this.yearOfBirth);
+  },
+};
+
+// create an object of type personProto
+var john = Object.create(personProto);
+// enter values for the instance
+john.name = "John";
+john.yearOfBirth = 1998;
+john.job = "teacher";
+
+// alternate way to use Object.create
+var jane = Object.create(personProto, {
+  name: { value: "Jane" },
+  yearOfBirth: { value: 1969 },
+  job: [(value: "designer")],
+});
+
+// The main difference between function constructor and Object.create
+// In Object.create the object inherits from the prototype object passed to create method
+// whereas, in function constructor, the object inherits from the prototype property of the
+// function coonstructor.
+// The main benefit of Object.create allows for implementation of complex inheritance.
+ */

ee8446cd9b52753e21063afa463977bcf6e3c1b7
Sandeep Singh <s.sandeep79@ymail.com>
2020-07-16 15:04:35 +0530

    coding challenge 7 - own solution (including expert level)

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -300,3 +300,110 @@ function calc_age(birth_year) {
 console.log(array_calc(years, calc_age));
 console.log(array_calc(array_calc(years, calc_age), isOfAge.bind(this, 20)));
  */
+
+/////////////////////////////
+// Coding Challenge 7
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+2. Create a couple of questions using the constructor
+3. Store them all inside an array
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+6. Check if the answer is correct and print to the console whether the answer is correct or not (Hint: write another method for this).
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+/* 
+////// Personal Solution
+var Question = function (question_num, question, answers, correct_answer) {
+  this.question = question;
+  this.answers = answers;
+  this.correct_answer = correct_answer;
+  this.question_num = question_num;
+};
+
+function select_random_question(total_questions) {
+  const random_question_index = Math.floor(Math.random() * total_questions);
+  console.log(random_question_index);
+  return random_question_index;
+}
+
+var questions = new Array();
+var total_questions = questions.push(
+  new Question(
+    1,
+    "How does selenium work?",
+    ["Json Wire Protocol (JWP)", "Javascript", "Java", "Direct injection"],
+    0
+  ),
+  new Question(
+    2,
+    "What is TestNG?",
+    ["API", "Test Framework", "Junit", "Browser"],
+    1
+  ),
+  new Question(
+    3,
+    "What is the usecase for Maven?",
+    ["Build lifecycle", "Project creation", "Testing", "Development"],
+    0
+  ),
+  new Question(
+    4,
+    "Most Important feature of a framework?",
+    ["Reporting", "screenshots", "analytics", "Video Recording"],
+    0
+  )
+);
+
+var user_answer_count = 0;
+var questions_answered = 0;
+
+do {
+  var selected_question_index = select_random_question(total_questions);
+  var selected_question = questions[selected_question_index];
+
+  console.log(
+    `${selected_question.question_num}: ${selected_question.question}`
+  );
+
+  var answers = selected_question.answers;
+  // console.log(answers);
+  for (var i = 0; i < answers.length; ++i) {
+    console.log(`${i}: ${answers[i]}`);
+  }
+
+  var user_answer = prompt(
+    "Please select the correct answer (just type the number). Enter exit to quit."
+  );
+  if (user_answer === "exit") break;
+  is_answer_correct(selected_question, parseInt(user_answer));
+} while (true);
+
+function is_answer_correct(selected_question, user_answer) {
+  ++questions_answered;
+  const answer = selected_question.correct_answer;
+  console.log(`You entered: ${user_answer}. Correct Answer: ${answer}.`);
+  if (user_answer === answer) {
+    console.log(
+      `${++user_answer_count} of ${questions_answered} answered correctly!`
+    );
+  } else
+    console.log(
+      `${user_answer_count} of ${questions_answered} answered correctly!`
+    );
+}
+ */
+
+/*
+--- Expert level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+11. Display the score in the console. Use yet another method for this.
+*/

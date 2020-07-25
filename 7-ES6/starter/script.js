f8f094d10e146ea2677c5a4050b3015d46d0c0ee
Sandeep Singh <s.sandeep79@ymail.com>
2020-07-25 11:01:16 +0530

    ES6 - Let and Const variable declaration

---
+++ 7-ES6/starter/script.js
@@ -0,0 +1,54 @@
+/////////////////////////////////
+// Lecture: let and const
+/* 
+ES5
+var name5 = "Jane Smith";
+var age5 = 23;
+name5 = "Jane Miller";
+
+console.log(name5);
+
+// ES6
+const name6 = "Jane Smith";
+let age6 = 23;
+name6 = "Jane Miller";
+console.log(name6);
+
+// ES5
+function driver_license5(is_test_cleared) {
+  if (is_test_cleared) {
+    console.log(first_name);
+    var first_name = "John";
+    var birth_year = 1990;
+
+    console.log(
+      `${first_name} born in ${birth_year} is now officially allowed to drive a car.`
+    );
+  }
+}
+driver_license5(true);
+
+// ES6
+function driver_license6(is_test_cleared) {
+  let first_name;
+  const birth_year = 1990; // const has to be defined at the time of declaration itself.
+  if (is_test_cleared) {
+    //console.log(first_name);
+    first_name = "John";
+    //birth_year = 1990; // this does not work as we are defining the const after declaration.
+
+    console.log(
+      `${first_name} born in ${birth_year} is now officially allowed to drive a car.`
+    );
+  }
+}
+driver_license6(true);
+
+let i = 23;
+for (let i = 0; i < 5; ++i) {
+    console.log(i);
+}
+
+console.log(i);
+// as 'let' variables are block scoped the value of i is limited within the for loop. 
+*/

839ecc269c8d58c58c0fbf42c99ea36d77b65931
Sandeep Singh <s.sandeep79@ymail.com>
2020-10-01 10:39:55 +0530

    Sec 9 - Forkify - initial commit

---
+++ 9-forkify/starter/src/js/models/Recipe.js
@@ -0,0 +1,1 @@
+// const res = await axios(`https://forkify-api.herokuapp.com/get?rId=${this.query}`)
---
+++ 9-forkify/starter/src/js/models/Search.js
@@ -0,0 +1,1 @@
+// const res = await axios(`https://forkify-api.herokuapp.com/search?&q=${this.query}`)
